aa5a5a15bc36282686bf7a22d73015d433c94859
Justyna Chowaniec (juchowan) <juchowan@cisco.com>
2022-12-12 15:09:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,12 @@ resource "aci_rest_managed" "licenseLicPolicy" {
   dn         = "uni/fabric/licensepol"
   class_name = "licenseLicPolicy"
   content = {
-    ipAddr     = var.proxy_hostname_ip
-    mode       = var.mode
-    port       = var.proxy_port
-    regTokenId = var.registration_token
-    url        = var.url
+    ipAddr        = var.proxy_hostname_ip
+    mode          = var.mode
+    port          = var.proxy_port
+    regTokenId    = var.registration_token
+    url           = var.url
+    regAdminState = "register"
   }
 }
 
